e92483151dcba0ffb9c7b0682c01440beceacaca
dtfgenetics <green801028@gmail.com>
2026-06-30 19:32:08 -0500

    Configure production session cookie

--- server/src/middleware/session.ts
+++ server/src/middleware/session.ts
@@ -22,18 +22,22 @@ declare module "http" {
         };
     }
 }
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const sessionMiddleware = session({
     store: new PGSession({ pool: db, createTableIfMissing: true }),
     secret: process.env.SESSION_SECRET || "make sure to change this!",
     resave: false,
     saveUninitialized: false,
-    name: "chessu",
+    name: process.env.SESSION_COOKIE_NAME || "kush_kings_chess",
     proxy: true,
     cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        secure: process.env.NODE_ENV === "production" ? true : false,
+        secure: isProduction,
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+        sameSite: isProduction ? "none" : "lax",
+        domain: process.env.SESSION_COOKIE_DOMAIN || undefined
     },
     genid: function () {
         return nanoid(21);
