dc96697e63943c02e8b98c6ddbd9f6c352b3a5d4
dtfgenetics <green801028@gmail.com>
2026-06-30 19:38:59 -0500

    Add Kush piece asset registry

--- client/src/kushTheme.ts
+++ client/src/kushTheme.ts
@@ -12,3 +12,18 @@ export const KUSH_COPY = {
   joinLightSide: "Join as light side",
   joinDarkSide: "Join as dark side"
 } as const;
+
+export const KUSH_PIECE_ASSETS = {
+  wK: "/kush-pieces/light-king.svg",
+  wQ: "/kush-pieces/light-queen.svg",
+  wB: "/kush-pieces/light-bishop.svg",
+  wN: "/kush-pieces/light-knight.svg",
+  wR: "/kush-pieces/light-rook.svg",
+  wP: "/kush-pieces/light-pawn.svg",
+  bK: "/kush-pieces/dark-king.svg",
+  bQ: "/kush-pieces/dark-queen.svg",
+  bB: "/kush-pieces/dark-bishop.svg",
+  bN: "/kush-pieces/dark-knight.svg",
+  bR: "/kush-pieces/dark-rook.svg",
+  bP: "/kush-pieces/dark-pawn.svg"
+} as const;
