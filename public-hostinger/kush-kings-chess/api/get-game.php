6bdd222163907dd128588ebfdb7836a6981b9199
dtfgenetics <green801028@gmail.com>
2026-07-04 15:15:53 -0500

    Implement minimal get game endpoint

--- public-hostinger/kush-kings-chess/api/get-game.php
+++ public-hostinger/kush-kings-chess/api/get-game.php
@@ -1,4 +1,9 @@
 <?php
 declare(strict_types=1);
 require __DIR__ . '/db.php';
-kkc_json(['ok' => false, 'error' => 'Endpoint not finished yet.']);
+
+$input = kkc_body();
+$roomCode = kkc_room_code((string)($input['room_code'] ?? ''));
+$game = kkc_require_game($roomCode);
+
+kkc_json(['ok' => true, 'game' => kkc_public_game($game)]);
